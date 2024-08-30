220577eed8940faa795cba7546da5aa6678221bd
safaminhaj <safaminhaj11@gmail.com>
2024-08-30 20:38:24 +0530

    basic setup for product controller and router

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 //routers import
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -33,6 +34,7 @@ app.get("/api/v1", (req, res) => {
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleware);
 
